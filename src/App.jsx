eacfb662bc9f7bfeab5eaaa824dba4a346548aff
Nisma <20-41982-1@student.aiub.edu>
2023-10-19 01:12:28 +0600

    delete add part of client and without loading deleted final output showed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 
 
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
 
+import CoffeeCard from './components/CoffeeCard';
+
+import { useState } from "react";
 function App() {
+  const loadedCoffees=useLoaderData();
+  console.log(loadedCoffees);
+  const [coffees,setCoffees]=useState(loadedCoffees);
   
 
   return (
-    <>
+    <div className="m-20">
       
-      <h1 className='text-6xl text-purple-600'>Vite + React</h1>
+      <h1 className='text-4xl text-purple-600 text-center mb-4'>Hot Hot cold coffee:{coffees.length}</h1>
+      <div className='grid md:grid-cols-2 gap-4'>
+      {
+        coffees.map(coffee=><CoffeeCard
+           key={coffee._id}
+           coffees={coffees}
+            setCoffees={setCoffees}
+           coffee={coffee}></CoffeeCard>)
+      }
+      </div>
 
-    </>
+
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -12,14 +12,16 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    loader:()=>fetch('http://localhost:5000/coffee')
   },
   {
     path:"/addCoffee",
     element:<AddCoffee/>
   },
   {
-    path:"/updateCoffee",
-    element:<UpdateCoffee/>
+    path:"/updateCoffee/:id",
+    element:<UpdateCoffee/>,
+    loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
   }
 ]);
 
